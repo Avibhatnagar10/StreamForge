bf6747757ae824c9009bd2b205156be16d6d3c07
Avi Bhatnagar <91671078+Avibhatnagar10@users.noreply.github.com>
2024-10-14 17:54:09 +0530

    Update firebaseConfig.js

--- Client/app/firebaseConfig.js
+++ Client/app/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBmAsaKuBf6pAQAnX1gRAKGJag15ZIjuMI",
-  authDomain: "streamforge-atw80.firebaseapp.com",
-  projectId: "streamforge-atw80",
-  storageBucket: "streamforge-atw80.appspot.com",
-  messagingSenderId: "16132538424",
-  appId: "1:16132538424:web:2f72ad6dc3809b34e8e6cd",
-  measurementId: "G-ZHDGF5T7YQ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
